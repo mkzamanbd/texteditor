4ff9cf12a0504a6d6ba6f4957563adb3197c3f82
Zaman <zaman7u@gmail.com>
2021-06-07 11:37:47 +0600

    performance improvements and bug fixes

--- sample/upload.php
+++ sample/upload.php
@@ -9,13 +9,13 @@ if (move_uploaded_file($request["tmp_name"], $upload_file)) {
     echo json_encode([
         "uploaded" => true,
         "message" => "The file ". htmlspecialchars( basename( $request["name"])). " has been uploaded.",
-        "url" => "http://localhost/texteditor/example/" . $upload_file,
+        "url" => "http://localhost/texteditor/sample/" . $upload_file,
     ]);
 } else {
     echo json_encode([
         "uploaded" => false,
         "message" => "Sorry, there was an error uploading your file.",
-        "url" => "http://localhost/texteditor/example/warning.png",
+        "url" => "http://localhost/texteditor/sample/warning.png",
     ]);
 }
 
